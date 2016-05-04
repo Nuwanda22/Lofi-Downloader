fda27cdc018e069565b03959fb6785c694fad902
Nuwanda22 <njk230@gmail.com>
2016-05-04 13:00:42 +0900

    초기 커밋.

---
+++ Lofi Downloader/MainForm.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace Lofi_Downloader
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// 필수 디자이너 변수입니다.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 사용 중인 모든 리소스를 정리합니다.
+		/// </summary>
+		/// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 디자이너에서 생성한 코드
+
+		/// <summary>
+		/// 디자이너 지원에 필요한 메서드입니다. 
+		/// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.findButton = new System.Windows.Forms.Button();
+			this.downloadButton = new System.Windows.Forms.Button();
+			this.allProgressBar = new System.Windows.Forms.ProgressBar();
+			this.thisProgressBar = new System.Windows.Forms.ProgressBar();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.allProgressLabel = new System.Windows.Forms.Label();
+			this.thisProgressLabel = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.gettingImageProgressBar = new System.Windows.Forms.ProgressBar();
+			this.gettingImageLabel = new System.Windows.Forms.Label();
+			this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.ImageLocation = "http://img.naver.net/static/www/u/2013/0731/nmms_224940510.gif";
+			this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(265, 237);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(283, 12);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(381, 21);
+			this.textBox1.TabIndex = 1;
+			this.textBox1.Text = "http://lofi.e-hentai.org/g/930457/b6c426b0f9/";
+			// 
+			// findButton
+			// 
+			this.findButton.Location = new System.Drawing.Point(502, 226);
+			this.findButton.Name = "findButton";
+			this.findButton.Size = new System.Drawing.Size(75, 23);
+			this.findButton.TabIndex = 2;
+			this.findButton.Text = "Find";
+			this.findButton.UseVisualStyleBackColor = true;
+			this.findButton.Click += new System.EventHandler(this.findButton_Click);
+			// 
+			// downloadButton
+			// 
+			this.downloadButton.Location = new System.Drawing.Point(589, 226);
+			this.downloadButton.Name = "downloadButton";
+			this.downloadButton.Size = new System.Drawing.Size(75, 23);
+			this.downloadButton.TabIndex = 3;
+			this.downloadButton.Text = "Download";
+			this.downloadButton.UseVisualStyleBackColor = true;
+			this.downloadButton.Click += new System.EventHandler(this.downloadButton_Click);
+			// 
+			// allProgressBar
+			// 
+			this.allProgressBar.Location = new System.Drawing.Point(283, 116);
+			this.allProgressBar.Name = "allProgressBar";
+			this.allProgressBar.Size = new System.Drawing.Size(381, 23);
+			this.allProgressBar.TabIndex = 4;
+			// 
+			// thisProgressBar
+			// 
+			this.thisProgressBar.Location = new System.Drawing.Point(283, 173);
+			this.thisProgressBar.Name = "thisProgressBar";
+			this.thisProgressBar.Size = new System.Drawing.Size(381, 23);
+			this.thisProgressBar.TabIndex = 5;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(283, 101);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(74, 12);
+			this.label1.TabIndex = 6;
+			this.label1.Text = "All Progress";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(281, 158);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(85, 12);
+			this.label2.TabIndex = 7;
+			this.label2.Text = "This Progress";
+			// 
+			// allProgressLabel
+			// 
+			this.allProgressLabel.Location = new System.Drawing.Point(591, 142);
+			this.allProgressLabel.Name = "allProgressLabel";
+			this.allProgressLabel.Size = new System.Drawing.Size(73, 12);
+			this.allProgressLabel.TabIndex = 8;
+			this.allProgressLabel.Text = "( 0 / 100 )";
+			this.allProgressLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// thisProgressLabel
+			// 
+			this.thisProgressLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.thisProgressLabel.Location = new System.Drawing.Point(608, 199);
+			this.thisProgressLabel.Name = "thisProgressLabel";
+			this.thisProgressLabel.Size = new System.Drawing.Size(56, 12);
+			this.thisProgressLabel.TabIndex = 9;
+			this.thisProgressLabel.Text = "( 0 % )";
+			this.thisProgressLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(283, 44);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(83, 12);
+			this.label5.TabIndex = 10;
+			this.label5.Text = "Getting Image";
+			// 
+			// gettingImageProgressBar
+			// 
+			this.gettingImageProgressBar.Location = new System.Drawing.Point(283, 59);
+			this.gettingImageProgressBar.Name = "gettingImageProgressBar";
+			this.gettingImageProgressBar.Size = new System.Drawing.Size(381, 23);
+			this.gettingImageProgressBar.TabIndex = 11;
+			// 
+			// gettingImageLabel
+			// 
+			this.gettingImageLabel.Location = new System.Drawing.Point(591, 85);
+			this.gettingImageLabel.Name = "gettingImageLabel";
+			this.gettingImageLabel.Size = new System.Drawing.Size(73, 12);
+			this.gettingImageLabel.TabIndex = 12;
+			this.gettingImageLabel.Text = "( 0 / 100 )";
+			this.gettingImageLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(676, 261);
+			this.Controls.Add(this.gettingImageLabel);
+			this.Controls.Add(this.gettingImageProgressBar);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.thisProgressLabel);
+			this.Controls.Add(this.allProgressLabel);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.thisProgressBar);
+			this.Controls.Add(this.allProgressBar);
+			this.Controls.Add(this.downloadButton);
+			this.Controls.Add(this.findButton);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.pictureBox1);
+			this.Name = "MainForm";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button findButton;
+		private System.Windows.Forms.Button downloadButton;
+		private System.Windows.Forms.ProgressBar allProgressBar;
+		private System.Windows.Forms.ProgressBar thisProgressBar;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label allProgressLabel;
+		private System.Windows.Forms.Label thisProgressLabel;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.ProgressBar gettingImageProgressBar;
+		private System.Windows.Forms.Label gettingImageLabel;
+		private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+	}
+}
+
---
+++ Lofi Downloader/MainForm.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Diagnostics;
+
+namespace Lofi_Downloader
+{
+	public partial class MainForm : Form
+	{
+		WebClient webClient;
+		string firstPage;
+		string thumbNail;
+		int pageCount;
+
+		public MainForm()
+		{
+			InitializeComponent();
+
+			webClient = new WebClient();
+			webClient.DownloadFileCompleted += (sender, e) =>
+			{
+				allProgressBar.Value++;
+				allProgressLabel.Text = ("( " + allProgressBar.Value + " / " + allProgressBar.Maximum + " )");
+			};
+			webClient.DownloadProgressChanged += (sender, e) =>
+			{
+				thisProgressBar.Value = e.ProgressPercentage;
+				thisProgressLabel.Text = ("( "+ e.ProgressPercentage+" % )");
+			};
+		}
+	
+		private void findButton_Click(object sender, EventArgs e)
+		{
+			gettingImageProgressBar.Value = 0;
+			allProgressBar.Value = 0;
+			thisProgressBar.Value = 0;
+
+			string html = getHTML(textBox1.Text);
+			string element = getElementByTag(html, "a");
+
+			thumbNail = getImageLink(element);
+			pictureBox1.ImageLocation = thumbNail;
+
+			firstPage = getAttribute(element, "href");
+
+			int max = getPageCount();
+			gettingImageProgressBar.Maximum = max;
+			allProgressBar.Maximum = max;
+
+			gettingImageLabel.Text = ("( " + 0 + " / " + gettingImageProgressBar.Maximum + " )");
+			allProgressLabel.Text = ("( " + 0 + " / " + allProgressBar.Maximum + " )");
+
+			MessageBox.Show("Finding Success!");
+		}
+
+		private int getPageCount()
+		{
+			string html = getHTML(firstPage);
+			string trElement = getElementByTag(html, "tr");
+			string temp = trElement.Substring(trElement.IndexOf("<td", trElement.IndexOf("<td") + 1));
+			string tdElement = getElementByTag(temp, "td");
+			
+			return pageCount = Convert.ToInt32(tdElement.Substring(tdElement.LastIndexOf('/') + 1));
+		}
+
+		private string[] getImageArray()
+		{
+			List<string> list = new List<string>();
+			string thisPage = firstPage;
+			string prevPage = null;
+
+			gettingImageProgressBar.Value = 0;
+			while (!thisPage.Equals(prevPage))
+			{
+				string html = getHTML(thisPage);
+				string element = getElementByTag(html, "a");
+
+				list.Add(getImageLink(element));
+
+				prevPage = thisPage;
+				thisPage = getAttribute(element, "href");
+
+				gettingImageProgressBar.Value++;
+				gettingImageLabel.Text = ("( " + gettingImageProgressBar.Value + " / " + gettingImageProgressBar.Maximum + " )");
+			}
+
+			return list.ToArray();
+		}
+
+		private string getHTML(string url)
+		{
+			string html;
+
+			HttpWebRequest request = WebRequest.CreateHttp(url);
+			HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+			// 응답이 OK면
+			if (response.StatusCode == HttpStatusCode.OK)
+			{
+				// 수신 스트림을 만들고
+				Stream receiveStream = response.GetResponseStream();
+				StreamReader readStream;
+
+				// 만약 캐릭터셋이 없으면 
+				if (response.CharacterSet == null)
+				{
+					// 없는 채로 만들고
+					readStream = new StreamReader(receiveStream);
+				}
+				else // 있으면
+				{
+					// 그 캐릭터 셋으로 인코딩을 정해
+					readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+				}
+				// 스트림리더를 만든다.
+
+				// 그 스트림 리더로부터 html을 받는다
+				html = readStream.ReadToEnd();
+
+				response.Close();
+				readStream.Close();
+
+				return html;
+			}
+			else // 응답이 OK가 아니면
+			{
+				throw new ArgumentException("Http Status Code : " + response.StatusCode.ToString());
+			}
+		}
+
+		private string getBody(string html)
+		{
+			string temp;
+
+			if (html.Contains("<body"))
+			{
+				int start = html.IndexOf("<body");
+				int end = html.IndexOf("</body>");
+				temp = html.Substring(start, end - start);
+
+				return temp;
+			}
+			else
+			{
+				return html;
+			}
+		}
+		
+		private string getElementByTag(string html, string tag)
+		{
+			string temp;
+			string body = getBody(html);
+			
+			if (body.Contains("<" + tag))
+			{
+				int start = body.IndexOf("<" + tag);
+				int end = body.IndexOf("</" + tag + ">");
+				temp = body.Substring(start, end - start);
+				
+				return temp;
+			}
+			else
+			{
+				return null;
+			}
+		}
+
+		private string getAttribute(string element, string attribute)
+		{
+			string temp = element.Substring(element.IndexOf(attribute));
+
+			return temp.Substring(temp.IndexOf('"') + 1, temp.IndexOf('"', temp.IndexOf('"') + 1) - attribute.Length - 2); ;
+		}
+
+		private string getImageLink(string element)
+		{
+			if (element.Contains("<img"))
+			{
+				int imgTag = element.IndexOf("src", element.IndexOf("<img"));
+
+				int imgLinkStart = element.IndexOf('"', imgTag) + 1;
+				int imgLinkEnd = element.IndexOf('"', imgLinkStart);
+
+				return element.Substring(imgLinkStart, imgLinkEnd - imgLinkStart);
+			}
+			else
+			{
+				return "nothing";
+			}
+		}
+
+		private void downloadButton_Click(object sender, EventArgs e)
+		{
+			folderBrowserDialog.Description = "Select folder!";
+			if(folderBrowserDialog.ShowDialog() == DialogResult.OK)
+			{
+				Task task = downloadTask(folderBrowserDialog.SelectedPath);
+			}
+		}
+
+		private async Task downloadTask(string path)
+		{
+			foreach (string s in getImageArray())
+			{
+				await downloadImage(s, path);
+			}
+			MessageBox.Show("Download Complete!");
+			Process.Start(path);
+		}
+
+		private async Task downloadImage(string url, string path)
+		{
+			string fileName = Path.Combine(path, url.Substring(url.LastIndexOf('/') + 1));
+
+			await webClient.DownloadFileTaskAsync(new Uri(url), fileName);
+			
+			webClient.Dispose();
+		}
+	}
+}
